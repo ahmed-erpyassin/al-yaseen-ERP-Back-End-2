5956c93b9b2e4a94243bab6b072bef56220d7734
salam-hammad <salamhammad2003@gmail.com>
2025-10-02 22:49:51 +0300

    Test Servise Controller

--- Modules/FinancialAccounts/database/seeders/FinancialAccountsDatabaseSeeder.php
+++ Modules/FinancialAccounts/database/seeders/FinancialAccountsDatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Modules\FinancialAccounts\database\seeders;
 use Illuminate\Database\Seeder;
 use Modules\FinancialAccounts\Database\Seeders\FinancialAccountsCoreSeeder;
 use Modules\FinancialAccounts\Database\Seeders\TaxRateSeeder;
+use Modules\FinancialAccounts\Database\Seeders\AccountGroupSeeder;
+use Modules\FinancialAccounts\Database\Seeders\AccountSeeder;
 
 class FinancialAccountsDatabaseSeeder extends Seeder
 {
@@ -15,7 +17,9 @@ class FinancialAccountsDatabaseSeeder extends Seeder
     {
         $this->call([
             FinancialAccountsCoreSeeder::class,
-            TaxRateSeeder::class,
+            AccountGroupSeeder::class,
+            AccountSeeder::class,
+            // TaxRateSeeder::class, // Commented out due to enum issue
         ]);
     }
 }
