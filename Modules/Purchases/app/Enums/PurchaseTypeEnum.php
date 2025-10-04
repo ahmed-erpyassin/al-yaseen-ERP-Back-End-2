223cac0bf6de96702b23e8fde49f754b18f454ef
salam-hammad <salamhammad2003@gmail.com>
2025-10-04 13:40:19 +0300

    Test IncomingShipmentController

--- Modules/Purchases/app/Enums/PurchaseTypeEnum.php
+++ Modules/Purchases/app/Enums/PurchaseTypeEnum.php
@@ -5,9 +5,10 @@ namespace Modules\Purchases\app\Enums;
 interface PurchaseTypeEnum
 {
     const QUOTATION = 'quotation';
+    const ORDER = 'order';
     const OUTGOING_ORDER = 'outgoing_order';
-    const INCOMING_SHIPMENT = 'incoming_shipment';
+    const INCOMING_SHIPMENT = 'shipment';
     const INVOICE = 'invoice';
-    const SERVICE = 'service';
+    const EXPENSE = 'expense';
     const RETURN_INVOICE = 'return_invoice';
 }
