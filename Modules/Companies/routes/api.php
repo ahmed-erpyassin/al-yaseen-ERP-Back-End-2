d79c670121551ddb7b0d636d3d875cbad3dbf867
eng-amroakram <eng.amroakram@gmail.com>
2025-10-07 09:53:36 +0300

    new fixes

--- Modules/Companies/routes/api.php
+++ Modules/Companies/routes/api.php
@@ -11,13 +11,15 @@ use Modules\Companies\Http\Controllers\RegionsController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
 
-    Route::apiResource('countries', CountriesController::class)->names('countries');
-    Route::apiResource('regions', RegionsController::class)->names('regions');
-    Route::apiResource('cities', CitiesController::class)->names('cities');
+    Route::withoutMiddleware(['auth:sanctum'])->group(function () {
+        Route::apiResource('countries', CountriesController::class)->names('countries');
+        Route::apiResource('regions', RegionsController::class)->names('regions');
+        Route::apiResource('cities', CitiesController::class)->names('cities');
 
-    Route::apiResource('industries', IndustriesController::class)->names('industries');
-    Route::apiResource('business-types', BusinessTypesController::class)->names('business_types');
+        Route::apiResource('industries', IndustriesController::class)->names('industries');
+        Route::apiResource('business-types', BusinessTypesController::class)->names('business_types');
 
-    Route::apiResource('companies', CompaniesController::class)->names('companies');
-    Route::apiResource('branches', BranchesController::class)->names('branches');
+        Route::apiResource('companies', CompaniesController::class)->names('companies');
+        Route::apiResource('branches', BranchesController::class)->names('branches');
+    });
 });
